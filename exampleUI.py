afba9c0b866538d38ee8237bf82833011ef1bd9b
+Ter <7396589+masterPlusTer@users.noreply.github.com>
2024-12-29 15:47:18 -0400

    Create exampleUI.py

---
+++ exampleUI.py
@@ -0,0 +1,15 @@
+import display
+import interfaceUI
+
+
+
+display.init_display()
+display.fill_screen(0b0000000000000000)  # limpia pantalla 
+display.set_rotation(1)  
+
+
+
+
+
+
+interfaceUI.draw_interface()
